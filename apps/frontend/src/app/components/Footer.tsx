6f3cf3917becf7362cfbb1a60ba9796d2ad96a4c
JohanGJensen <johancentralen@gmail.com>
2024-04-01 13:04:14 +0200

    typo fix

--- apps/frontend/src/app/components/Footer.tsx
+++ apps/frontend/src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className="flex items-center justify-center w-full h-14 bg-nav-primary p-5 text-white fixed bottom-0">
       <span className="text-[8px] md:text-xs">
-        © 1996-2024, Washaton.com, Inc. or its affiliates
+        © 1996-2024, Washazon.com, Inc. or its affiliates
       </span>
     </footer>
   );
