95d7357d1087c14849b7b6df473f09bed1bef10b
JohanGJensen <johancentralen@gmail.com>
2024-04-01 14:26:04 +0200

    add basket page - initial draft

---
+++ apps/frontend/src/app/basket/page.tsx
@@ -0,0 +1,105 @@
+"use client"
+
+import React from "react";
+import { HiTrash } from "react-icons/hi";
+import PageWrapper from "../components/PageWrapper";
+import Image from "next/image";
+
+interface BasketPageProps {}
+
+type BasketProduct = {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  amount: number;
+};
+
+const items: BasketProduct[] = [
+  {
+    id: 1,
+    name: "item 1",
+    image: "https://placeholder.com/100",
+    price: 250,
+    amount: 1,
+  },
+  {
+    id: 2,
+    name: "item 2",
+    image: "https://placeholder.com/100",
+    price: 299,
+    amount: 1,
+  },
+  {
+    id: 3,
+    name: "item 3",
+    image: "https://placeholder.com/100",
+    price: 50,
+    amount: 1,
+  },
+  {
+    id: 4,
+    name: "item 4",
+    image: "https://placeholder.com/100",
+    price: 300,
+    amount: 1,
+  },
+];
+
+const BasketPage: React.FC<BasketPageProps> = () => {
+  const [basketItems, setBasketItems] = React.useState(items);
+
+  const onRemoveItem = (id: number) => {
+    const index = basketItems.findIndex((item) => item.id === id);
+    const newBasketItems = basketItems;
+    newBasketItems.splice(index, 1);
+
+    setBasketItems([...newBasketItems]);
+  };
+
+  return (
+    <PageWrapper>
+      <section className="basket-overview-section">
+        <div className={"w-2/5 bg-gray-50 p-10"}>
+          {basketItems.map((item) => (
+            <div
+              key={item.id}
+              className="w-full flex flex-wrap justify-between border-b-[1px] border-nav-primary mb-3 pb-2"
+            >
+              <div className={"flex justify-start"}>
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={100}
+                  height={100}
+                />
+                <div className="lg:w-1/2 w-full pl-3">
+                  <h1 className="text-gray-900 text-2xl font-medium">
+                    {item.name}
+                  </h1>
+
+                  <div className="flex flex-wrap">
+                    <span className="text-gray-700">{item.price},-</span>
+                  </div>
+                  {/* <div className="flex flex-wrap">
+                  <span className="text-2xl text-gray-900">
+                    {item.price},-
+                  </span>
+                </div> */}
+                </div>
+              </div>
+
+              <div>
+                <button onClick={() => onRemoveItem(item.id)}>
+                  <HiTrash color={"#d42828"} size={20} />
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </PageWrapper>
+  );
+};
+
+export default BasketPage;
