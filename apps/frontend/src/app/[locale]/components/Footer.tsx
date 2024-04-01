99f0ba2b067d0bea0438dc77b2eade7666e64350
JohanGJensen <johancentralen@gmail.com>
2024-04-01 12:58:09 +0200

    set nav background default color in config file

--- apps/frontend/src/app/[locale]/components/Footer.tsx
+++ apps/frontend/src/app/[locale]/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC<FooterProps> = () => {
   const t = useTranslations("general.company");
 
   return (
-    <footer className="flex items-center justify-center w-full h-14 bg-[#131921] p-5 text-white fixed bottom-0">
+    <footer className="flex items-center justify-center w-full h-14 bg-nav-primary p-5 text-white fixed bottom-0">
       <span className="text-[8px] md:text-xs">{t("trademark")}</span>
     </footer>
   );
